1a51ff4b1f6d93d91477fdb54f417a4bd4c48312
Boba Fat <ivan@workisfun.ru>
2008-08-20 23:57:36 +0400

    initial

---
+++ lib/access.rb
@@ -0,0 +1,106 @@
+module Access
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def allow(*arguments)
+      add_access_rule(:allow, arguments, arguments.extract_options!)
+    end
+
+    def deny(*arguments)
+      add_access_rule(:deny, arguments, arguments.extract_options!)
+    end
+
+    def add_access_rule(type, actions, options)
+      if [:allow, :deny].include?(type)
+        if read_inheritable_attribute(:access_rules).nil?
+          before_filter :access_filter
+          write_inheritable_attribute(:access_rules, [])
+        end
+
+        rules = if actions.length > 0
+          actions.map do |action|
+            {:type => type, :action => action, :options => options}
+          end
+        else
+          [{:type => type, :options => options}]
+        end
+        write_inheritable_array(:access_rules, rules)
+      end
+    end
+
+    def access_rules
+      read_inheritable_attribute(:access_rules)
+    end
+
+    def allow_by_default
+      set_default_access(:allow)
+    end
+
+    def deny_by_default
+      set_default_access(:deny)
+    end
+
+    def set_default_access(type)
+      if read_inheritable_attribute(:default_access).nil?
+        before_filter :default_access_filter
+      end
+      write_inheritable_attribute(:default_access, type == :allow)
+    end
+    
+    def default_access
+      read_inheritable_attribute(:default_access)
+    end
+
+  end
+
+  def access_denied
+    redirect_to('/')
+  end
+
+private
+
+  def default_access_filter
+    if self.class.access_rules.nil? && self.class.default_access == false
+      access_denied
+    end
+  end
+
+  def access_filter
+    action = action_name.to_sym
+
+    allow = self.class.default_access
+    render_tmpl = nil
+    # callback = nil
+
+    self.class.access_rules.each do |rule|
+      if rule[:action].nil? || rule[:action] == action
+        render_tmpl ||= rule[:options][:render]
+        # callback ||= rule[:options][:callback]
+
+        pass_block = if rule[:options][:if]
+          rule[:options][:if].is_a?(Symbol) ? send(rule[:options][:if]) : rule[:options][:if].call(self)
+        elsif rule[:options][:unless]
+          !(rule[:options][:unless].is_a?(Symbol) ? send(rule[:options][:unless]) : rule[:options][:unless].call(self))
+        else
+          true
+        end
+
+        if pass_block
+          allow = rule[:type] === :allow ? true : false
+        end
+      end
+    end
+
+    if allow == false
+      if render_tmpl
+        render :action => render_tmpl, :layout => 'error', :status => 401
+      # elsif callback
+      #   callback.is_a?(Symbol) ? send(callback) : callback.call(self)
+      else
+        access_denied
+      end
+    end
+  end
+end
---
+++ spec/access_spec.rb
@@ -0,0 +1,305 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+module AccessModuleSpecHelper
+  def request
+    @request
+  end
+
+  def create_controller(&block)
+    controller = Class.new(ApplicationController)
+    controller.class_eval do
+      def first
+        render :text => 'First!'
+      end
+
+      def second
+        render :text => 'Second!'
+      end
+
+      def third
+        render :text => 'Third!'
+      end
+    end
+    controller.class_eval(&block) if block
+    @controller = controller.new
+    controller
+  end
+
+  def create_sub_controller(&block)
+    sub_controller = Class.new(@controller.class)
+    sub_controller.class_eval(&block) if block
+    @controller = sub_controller.new
+    sub_controller
+  end
+
+  def pass_test(*actions)
+    %w(first second third).each do |action|
+      if actions.include?(action.to_sym) || actions == [:all] || actions == []
+        send("#{action}_should_pass")
+      else
+        send("#{action}_should_not_pass")
+      end
+    end
+  end
+
+  def method_missing(method, *args, &block)
+    if m = method.to_s.match(/^(first|second|third)_should(_not)?_pass$/)
+      # first_should_pass, first_should_not_pass, second_should_pass, second_should_not_pass, third_should_pass, third_should_not_pass
+      get m[1]
+      unless m[2]
+        @response.should have_text("#{m[1].camelize}!")
+      else
+        @response.should redirect_to('/')
+      end
+    else
+      super(method, *args, &block)
+    end
+  end
+
+end
+
+describe Access do
+  include AccessModuleSpecHelper
+  before(:each) do
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  it "should allow without rules" do
+    create_controller
+
+    pass_test(:all)
+  end
+
+  it "should allow for global allow" do
+    create_controller do
+      allow
+    end
+
+    pass_test(:all)
+  end
+
+  it "should deny for global deny" do
+    create_controller do
+      deny
+    end
+
+    pass_test(:none)
+  end
+
+  it "should handle \"silly\" rules (example 1)" do
+    create_controller do
+      allow
+      allow
+      allow
+    end
+
+    pass_test(:all)
+  end
+
+  it "should handle \"silly\" rules (example 2)" do
+    create_controller do
+      allow
+      deny
+      allow
+    end
+
+    pass_test(:all)
+  end
+
+end
+
+describe Access, 'with individual rules' do
+  include AccessModuleSpecHelper
+  before(:each) do
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  it "should allow all if allow rule applies only to one action" do
+    create_controller do
+      allow :first
+    end
+
+    pass_test(:all)
+  end
+
+  it "should deny only one action if deny rule applies only to that action" do
+    create_controller do
+      deny :first
+    end
+
+    pass_test(:second, :third)
+  end
+
+  it "should deny all if last rule is global deny" do
+    create_controller do
+      allow :first
+      deny
+    end
+
+    pass_test(:none)
+  end
+
+  it "should allow only one action if rule allowing it goes after global deny" do
+    create_controller do
+      deny
+      allow :first
+    end
+
+    pass_test(:first)
+  end
+
+  it "should handle multiple rules" do
+    create_controller do
+      deny :first
+      deny :third
+    end
+
+    pass_test(:second)
+  end
+
+  it "should handle multiple actions per rule" do
+    create_controller do
+      deny :first, :third
+    end
+
+    pass_test(:second)
+  end
+
+  it "should handle long dumb list of rules" do
+    create_controller do
+      # + + +
+      deny :first, :third
+      # - + -
+      allow :first, :second
+      # + + -
+      deny :second, :third
+      # + - -
+      allow :first, :third
+      # + - +
+      deny :first, :second
+      # - - +
+      allow :second, :third
+      # - + +
+    end
+
+    pass_test(:second, :third)
+  end
+
+end
+
+describe Access, 'with if and unless' do
+  include AccessModuleSpecHelper
+  before(:each) do
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  it "should apply rule if function evaluates to true" do
+    create_controller do
+      deny :if => :first?
+
+      def first?
+        action_name == 'first'
+      end
+    end
+
+    pass_test(:second, :third)
+  end
+
+  it "should apply rule if inline block evaluates to true" do
+    create_controller do
+      deny :if => lambda{ |c| c.action_name == 'first' }
+    end
+
+    pass_test(:second, :third)
+  end
+
+  it "should apply rule unless function evaluates to true" do
+    create_controller do
+      deny :unless => :first?
+
+      def first?
+        action_name == 'first'
+      end
+    end
+
+    pass_test(:first)
+  end
+
+  it "should apply rule unless inline block evaluates to true" do
+    create_controller do
+      deny :unless => lambda{ |c| c.action_name == 'first' }
+    end
+
+    pass_test(:first)
+  end
+
+end
+
+describe Access, 'with default access' do
+  include AccessModuleSpecHelper
+  before(:each) do
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  it "should allow if allow_by_default" do
+    create_controller do
+      allow_by_default
+    end
+
+    pass_test(:all)
+  end
+
+  it "should deny if deny_by_default" do
+    create_controller do
+      deny_by_default
+    end
+
+    pass_test(:none)
+  end
+
+  it "should inherit allow_by_default" do
+    create_controller do
+      allow_by_default
+    end
+    create_sub_controller
+
+    pass_test(:all)
+  end
+
+  it "should inherit deny_by_default" do
+    create_controller do
+      deny_by_default
+    end
+    create_sub_controller
+
+    pass_test(:none)
+  end
+
+  it "should rewrite inherited allow_by_default" do
+    create_controller do
+      allow_by_default
+    end
+    create_sub_controller do
+      deny_by_default
+    end
+
+    pass_test(:none)
+  end
+
+  it "should rewrite inherited deny_by_default" do
+    create_controller do
+      deny_by_default
+    end
+    create_sub_controller do
+      allow_by_default
+    end
+
+    pass_test(:all)
+  end
+
+
+end
