45efd00f28e300ce1ee51a65eb90c40544cd5bfd
Boba Fat <ivan@workisfun.ru>
2009-09-20 00:19:28 +0400

    moved spec log to base app log folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,4 @@ rescue LoadError
 end
 
 plugin_spec_dir = File.dirname(__FILE__)
-ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
-
+ActiveRecord::Base.logger = Logger.new(File.join(RAILS_ROOT, 'log', 'access-plugin-debug.log'))
