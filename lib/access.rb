758126f10229b9cc958a953d6e32657ee027372a
Boba Fat <ivan@workisfun.ru>
2009-10-10 07:39:29 +0400

    made private methods private

--- lib/access.rb
+++ lib/access.rb
@@ -15,12 +15,30 @@ module Access
     CONDITION_RULE_OPTIONS = [:if, :if_any, :if_all, :unless, :unless_any, :unless_all]
     VALID_RULE_OPTIONS = [:render, :callback] + CONDITION_RULE_OPTIONS
 
+    def allow_by_default
+      set_default_access(:allow)
+    end
+
+    def deny_by_default
+      set_default_access(:deny)
+    end
+
+    def default_access
+      read_inheritable_attribute(:default_access)
+    end
+
+    def access_rules
+      read_inheritable_attribute(:access_rules)
+    end
+
+  private
+
     def add_access_rule(type, actions, options)
-      options.assert_valid_keys(VALID_RULE_OPTIONS) # TODO: test
-      condition_option_keys = options.keys & CONDITION_RULE_OPTIONS # TODO: test
-      if condition_option_keys.length > 1 # TODO: test
-        raise(ArgumentError, "There can be only one condition, got #{condition_option_keys.length}: #{condition_option_keys.join(", ")}") # TODO: test
-      end # TODO: test
+      options.assert_valid_keys(VALID_RULE_OPTIONS)
+      condition_option_keys = options.keys & CONDITION_RULE_OPTIONS
+      if condition_option_keys.length > 1
+        raise(ArgumentError, "There can be only one condition, got #{condition_option_keys.length}: #{condition_option_keys.join(", ")}")
+      end
 
       if [:allow, :deny].include?(type)
         if read_inheritable_attribute(:access_rules).nil?
@@ -39,29 +57,12 @@ module Access
       end
     end
 
-    def access_rules
-      read_inheritable_attribute(:access_rules)
-    end
-
-    def allow_by_default
-      set_default_access(:allow)
-    end
-
-    def deny_by_default
-      set_default_access(:deny)
-    end
-
     def set_default_access(type)
       if read_inheritable_attribute(:default_access).nil?
         before_filter :default_access_filter
       end
       write_inheritable_attribute(:default_access, type == :allow)
     end
-
-    def default_access
-      read_inheritable_attribute(:default_access)
-    end
-
   end
 
   def access_denied
